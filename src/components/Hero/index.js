07e8521d279c677d1a1903157f056e943695b5f1
DavidNabet <david97.nabet@gmail.com>
2021-05-11 20:33:04 +0200

    hero button token

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,15 +1,25 @@
 import React from "react";
 import brush from "../../assets/brush.svg";
 import "./index.css";
-
+import { Link } from "react-router-dom";
+import Cookies from "js-cookie";
 const Hero = () => {
+  const userToken = Cookies.get("userToken");
   return (
     <div className="home hero">
       <img src={brush} alt="" className="home-hero-brush" />
       <div className="container">
         <div className="home-hero-cta">
           <h1>Prêts à faire du tri dans vos placards ?</h1>
-          <button>Commencer à vendre</button>
+          {userToken ? (
+            <Link to="/publish">
+              <button>Commencer à vendre</button>
+            </Link>
+          ) : (
+            <Link to="/login">
+              <button>Commencer à vendre</button>
+            </Link>
+          )}
         </div>
       </div>
     </div>
